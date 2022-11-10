5a68a4714cd1a2c5148b9a054da28a11ee8437f7
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-11-10 00:10:44 -0300

    Renderizando Header em tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from './Components/Header/Header'
 import './global.css'
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
     </div>
   )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,10 @@
+import appName from '../../assets/appName.svg'
+import styles from './styles.module.css';
+
 export function Header() {
     return(
-        <header>
+        <header className={styles.headerContainer}>
+            <img src={appName} alt="Logotipo" />
         </header>
     );
 }
