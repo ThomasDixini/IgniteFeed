2c14716f30b39ea78582369f1cb0e89f0f6b1ebe
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-11-12 10:23:08 -0300

    SideBar renderizado e estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Header } from './Components/Header/Header'
+import { SideBar } from './Components/SideBar/SideBar'
 import './global.css'
+import styles from './styles.module.css'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita ex eveniet enim. Non, ducimus sint, a reprehenderit, error excepturi iure ab repellendus neque ex deleniti cupiditate dolorem quos nostrum blanditiis.</p>
+        </main>
+      </div>
     </div>
   )
 }
--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,9 +1,28 @@
 import styles from './styles.module.css';
+import {BsPencilSquare} from 'react-icons/bs'
 
 export function SideBar() {
     return(
         <aside className={styles.asideContainer}>
-
+            <img 
+            src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" 
+            alt="Foto de capa" 
+            className={styles.cover} 
+            />
+            <div className={styles.profile}>
+                <img src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50&q=80" 
+                alt="Foto do Perfil"
+                className={styles.avatar}
+                />
+                <strong> Diego Fernandes </strong>
+                <span> Web Developer </span>
+            </div>
+            <footer>
+                <a href="#">
+                    <BsPencilSquare/> 
+                    <p> Editar seu Perfil </p>
+                </a>
+            </footer>
         </aside>
     );
 }
