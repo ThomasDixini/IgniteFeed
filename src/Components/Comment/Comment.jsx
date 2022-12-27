761816e667048e478745cb25afda2059ee13810e
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-12-26 22:45:35 -0300

    Component comment Criado

---
+++ src/Components/Comment/Comment.jsx
@@ -0,0 +1,17 @@
+export function Comment() {
+    return(
+        <article>
+            <img src="https://github.com/diego3g.png" alt="Logo de Diego Fernandes" />
+            <div>
+                <header>
+                    <div>
+                        <strong> Diego Fernandes</strong>
+                        <span> Cerca de 2h atrás</span>
+                    </div>
+                    <BsFillTrashFill size={24}/>
+                </header>
+            </div>
+            <p> Muito bom Devon!! Gostei &hearts; </p>
+        </article>
+    );
+}
